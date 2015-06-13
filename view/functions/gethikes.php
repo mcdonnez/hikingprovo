3107e15b0c2d5c34cfca385b0be1b525fe790c51
mcdonnez <mailman966@gmail.com>
2015-06-13 21:53:29 +0000

    fully integrate sql on hikes.php

--- view/functions/gethikes.php
+++ view/functions/gethikes.php
@@ -8,11 +8,11 @@ if ($result->num_rows > 0) {
     // output data of each row
     $area = " ";
     while($row = $result->fetch_assoc()) {
-        if (strcmp($row["areaname"],$area) == 0) {
+        if (strcmp($row["areaname"],$area) != 0) {
+        echo "New Area: " . $row["areaname"] . "<br>\n";
         echo "id: " . $row["id"]. " - Name: " . $row["name"]. " Difficulty:" . $row["difficulty"]. "<br>\n";
         $area = $row["areaname"];
         } else {
-        echo "New Area: " . $row["areaname"] . "<br>\n";
         echo "id: " . $row["id"]. " - Name: " . $row["name"]. " Difficulty:" . $row["difficulty"]. "<br>\n";
         $area = $row["areaname"];
         }
